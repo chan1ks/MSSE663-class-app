dc6c42986b7e906c4259fe07d731429a69751473
morganistic <morgan@border.dev>
2020-10-12 14:20:26 -0600

    imports fix

--- src/app/store/application-initial-mock-state.ts
+++ src/app/store/application-initial-mock-state.ts
@@ -1,8 +1,8 @@
 import { ApplicationState } from './models/application-state.model';
 import { AUTH_INITIAL_MOCK_STATE } from './auth-initial-mock-state';
 import { USER_INITIAL_MOCK_STATE } from './user-initial-mock-state';
-import { HEROES_INITIAL_MOCK_STATE } from '../hero-list/store/heroes-initial-mock-state';
-import { HERO_INITIAL_MOCK_STATE } from '../hero-list/store/hero-initial-mock-state';
+import { HERO_INITIAL_MOCK_STATE } from '../heroes/store/hero-initial-mock-state';
+import { HEROES_INITIAL_MOCK_STATE } from '../heroes/store/heroes-initial-mock-state';
 
 export const APP_INTIAL_MOCK_STATE: ApplicationState = {
   router: null,
