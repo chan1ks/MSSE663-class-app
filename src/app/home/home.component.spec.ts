09183de90f3a35b15a7bf9b097a227f1c760d51c
morganistic <morgan@border.dev>
2020-10-12 12:56:40 -0600

    unit test fixes

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
+import { HeroesFacade } from '../heroes/heroes.facade';
+import { MOCK_STORE$ } from '../store/testing';
 
 import { HomeComponent } from './home.component';
 
@@ -9,6 +12,7 @@ describe('HomeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
+      providers: [HeroesFacade, { provide: Store, useValue: MOCK_STORE$ }],
     }).compileComponents();
   }));
 
